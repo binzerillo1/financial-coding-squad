700ed67591d7d335e48894a09e0f84e6d7765c99
binzerillo1 <brian.inzerillo@gmail.com>
2020-05-20 22:20:31 -0400

    updated fonts - actually happy with this rn

--- BriHub/launch.py
+++ BriHub/launch.py
@@ -93,48 +93,58 @@ root = tk.Tk()
 
 # set window title
 root.wm_title("Command Station")
-root.geometry("800x600") # length by width
+#root.geometry("800x600") # length by width
+# in between entries, spacing row
+tk.Label(root, text="", pady = 10).grid(row = 0, column = 1)
+
 
 for k in range(0, stocks_held):
     
     # col 0
-    tk.Label(root, text = stock_name[k], padx = 30).grid(row = k*4 + 1, column = 0)
+    tk.Label(root, text = stock_name[k], padx = 30, font=("Arial Black", 16)).grid(row = k*4+2, column = 0)
     
     # col 1
-    tk.Label(root, text = "Purchase Price:").grid(row = k*4, column = 1)
-    tk.Label(root, text = "Shares Purchased:").grid(row = k*4+1, column = 1)
-    tk.Label(root, text = "Purchase Date:").grid(row = k*4+2, column = 1)
+    tk.Label(root, text = "Purchase Price:", font=("Arial", 10)).grid(row = k*4+1, column = 1)
+    tk.Label(root, text = "Shares Purchased:", font=("Arial", 10)).grid(row = k*4+2, column = 1)
+    tk.Label(root, text = "Purchase Date:", font=("Arial", 10)).grid(row = k*4+3, column = 1)
     
     # col 2
-    tk.Label(root, text = "$" + str(buy_price[k])).grid(row = k*4, column = 2)
-    tk.Label(root, text = number_shares[k]).grid(row = k*4+1, column = 2)
-    tk.Label(root, text = buy_date[k]).grid(row = k*4+2, column = 2)
+    tk.Label(root, text = "$" + str(buy_price[k]), font=("Arial", 10)).grid(row = k*4+1, column = 2)
+    tk.Label(root, text = number_shares[k], font=("Arial", 10)).grid(row = k*4+2, column = 2)
+    tk.Label(root, text = buy_date[k], font=("Arial", 10)).grid(row = k*4+3, column = 2)
 
     # col 3
-    tk.Label(root, text = "Current Price:", padx = 10).grid(row = k*4, column = 3)
-    tk.Label(root, text = "Total Gain/Loss:", padx = 10).grid(row = k*4+1, column = 3)
-    tk.Label(root, text = "Length Held:", padx = 10).grid(row = k*4+2, column = 3)
+    tk.Label(root, text = "Current Price:", padx = 10, font=("Arial", 10)).grid(row = k*4+1, column = 3)
+    tk.Label(root, text = "Total Gain/Loss:", padx = 10, font=("Arial", 10)).grid(row = k*4+2, column = 3)
+    tk.Label(root, text = "Length Held:", padx = 10, font=("Arial", 10)).grid(row = k*4+3, column = 3)
     
     # col 4
-    tk.Label(root, text = current_price[k]).grid(row = k*4, column = 4)
-    tk.Label(root, text = "$" + str(profit[k])).grid(row = k*4+1, column = 4)
-    tk.Label(root, text = str(length_held[k]) + " days").grid(row = k*4+2, column = 4)
+    tk.Label(root, text = current_price[k], font=("Arial", 10)).grid(row = k*4+1, column = 4)
+    tk.Label(root, text = "$" + str(profit[k]), font=("Arial", 10)).grid(row = k*4+2, column = 4)
+    tk.Label(root, text = str(length_held[k]) + " days", font=("Arial", 10)).grid(row = k*4+3, column = 4)
 
     # col 5
-    tk.Label(root, text = "Percent Change: " + str(percent_change[k]) + "%", padx = 20).grid(row = k*4+1, column = 5)
+    if (percent_change[k] > 0):
+        tk.Label(root, text = "Percent Change: " + str(percent_change[k]) + "%", padx = 20, fg = 'green', font=("Arial", 12)).grid(row = k*4+2, column = 5)
+    else:
+        tk.Label(root, text = "Percent Change: " + str(percent_change[k]) + "%", padx = 20, fg = 'red', font=("Arial", 12)).grid(row = k*4+2, column = 5)
 
     # col 6
-    tk.Label(root, text = "Stop Price:", padx = 10).grid(row = k*4, column = 6)
-    tk.Label(root, text = "Stop Percent Change:", padx = 10).grid(row = k*4+1, column = 6)
-    tk.Label(root, text = "Days Until Earnings:", padx = 10).grid(row = k*4+2, column = 6)
+    tk.Label(root, text = "Stop Price:", padx = 5, font=("Arial", 10)).grid(row = k*4+1, column = 6)
+    tk.Label(root, text = "Stop Percent Change:", padx = 5, font=("Arial", 10)).grid(row = k*4+2, column = 6)
+    tk.Label(root, text = "Days Until Earnings:", padx = 5, font=("Arial", 10)).grid(row = k*4+3, column = 6)
     
     # col 7
-    tk.Label(root, text = "$" + str(stop_price[k])).grid(row = k*4, column = 7)
-    tk.Label(root, text = str(stop_percent[k]) + "%").grid(row = k*4+1, column = 7)
-    tk.Label(root, text = str(days_to_earnings[k]) + " days").grid(row = k*4+2, column = 7)
+    tk.Label(root, text = "$" + str(stop_price[k]), padx = 15, justify = 'left', font=("Arial", 10)).grid(row = k*4+1, column = 7)
+    
+    if (stop_percent[k] > 0):
+        tk.Label(root, text = str(stop_percent[k]) + "%", padx = 15, justify = 'left', font=("Arial", 10), fg = 'green').grid(row = k*4+2, column = 7)
+    else:
+        tk.Label(root, text = str(stop_percent[k]) + "%", padx = 15, justify = 'left', font=("Arial", 10), fg = 'red').grid(row = k*4+2, column = 7)
+    tk.Label(root, text = str(days_to_earnings[k]) + " days", padx = 15, justify = 'left', font=("Arial", 10)).grid(row = k*4+3, column = 7)
     
     # in between entries, spacing row
-    tk.Label(root, text="", pady = 10).grid(row = 4*k+3, column = 1)
+    tk.Label(root, text="", pady = 10).grid(row = 4*k+4, column = 1)
 
 
 # show window
